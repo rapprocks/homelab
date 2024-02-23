27a6ff253b5823ab0a436c4f96a066fd1166c074
rapprocks <rapprocks@users.noreply.github.com>
2024-02-23 07:12:15 +0100

    Basic config that works

--- main.tf
+++ main.tf
@@ -1,36 +1,38 @@
 provider "proxmox" {
-    pm_api_url = var.proxmox_host["pm_api_url"]
-    pm_user = var.proxmox_host["pm_user"]
-    pm_tls_insecure = true
+  pm_api_url          = var.proxmox_host["pm_api_url"]
+  pm_user             = var.proxmox_host["pm_user"]
+  pm_api_token_id     = var.proxmox_host["pm_api_token_id"]
+  pm_api_token_secret = var.proxmox_host["pm_api_token_secret"]
+  pm_tls_insecure     = true
 }
 
+variable "ct_name" {
+  description = "Container name"
+  type        = string
+  default     = "lxc"
+}
 
-resource "proxmox_lxc" "container" {
-  count = length(var.hostnames)
-  hostname = var.hostnames[count.index]
+resource "proxmox_lxc" "basic" {
+  hostname    = "${var.ct_name}-${var.naming_convention}"
   target_node = var.proxmox_host["target_node"]
-  vmid = var.vmid + count.index
-  cores = 1
-  memory = 2048
+  cores       = 1
+  memory      = 2048
 
-  ostemplate = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
-  ostype = "ubuntu"
+  ostemplate   = "docs:vztmpl/ubuntu-23.10-standard_23.10-1_amd64.tar.zst"
   unprivileged = "true"
-  start = "true"
+  start        = "true"
 
 
   rootfs {
-    storage = "local-lvm"
-    size = var.rootfs_size
+    storage = "vm_data"
+    size    = "8G"
   }
-  swap = 0
 
   network {
-    ip = format("%s/24", var.ips[count.index])
-    name ="eth0"
+    name   = "eth0"
     bridge = "vmbr0"
-    # firewall = "true" # this does not work at the moment
-    gw = cidrhost(format("%s/24", var.ips[count.index]), 1)
+    ip     = "dhcp"
+    ip6    = "auto"
   }
 
   ssh_public_keys = file(var.ssh_keys["pub"])
--- vars.tf
+++ vars.tf
@@ -1,45 +1,24 @@
 variable "proxmox_host" {
-	type = map
-     default = {
-       pm_api_url = "https://10.0.42.50:8006/api2/json"
-       pm_user = "root@pam"
-       target_node = "pve"
-     }
+  type = map(any)
+  default = {
+    pm_api_url          = "https://10.100.0.8:8006/api2/json"
+    pm_user             = "root@pam"
+    target_node         = "pve-1-prod"
+    pm_api_token_id     = "terraform-prov@pve!terraform"
+    pm_api_token_secret = "139d34c4-d8c0-4fff-9a39-1a7dd498a6c9"
+  }
 }
 
-variable "vmid" {
-	default     = 300
-	description = "Starting ID for the CTs"
-}
-
-
-variable "hostnames" {
+variable "naming_convention" {
   description = "Containers to be created"
-  type        = list(string)
-  default     = ["prod-ct"]
-}
-
-
-variable "rootfs_size" {
-	description = "Root filesystem size in GB"
-	default = "2G"
-}
-
-variable "ips" {
-    description = "IPs of the containers, respective to the hostname order"
-    type        = list(string)
-	default     = ["10.0.42.83"]
-}
-
-variable "user" {
-	default     = "root"
-	description = "Ansible user used to provision the container"
+  type        = string
+  default     = "pve-1"
 }
 
 variable "ssh_keys" {
-	type = map
-     default = {
-       pub = "~/.ssh/id_ed25519-pwless.pub"
-       priv = "~/.ssh/id_ed25519-pwless"
-     }
+  type = map(any)
+  default = {
+    pub  = "~/.ssh/nixdesk.pub"
+    priv = "~/.ssh/nixdesk"
+  }
 }
